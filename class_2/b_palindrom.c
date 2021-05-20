fa6f9b91c53343520877157ce7d3103ca516585a
Gyeongnam Kim <rudska252@naver.com>
2021-05-20 16:19:09 +0900

    fix-1259

--- class_2/b_palindrom.c
+++ class_2/b_palindrom.c
@@ -1,25 +1,9 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int	check_palin(int	len, char *arr)
-{
-	int	i;
-	
-	i = 0;
-	while (i < len)
-	{
-		if (arr[i] != arr[len - 1])
-			return (0);
-		i++;
-		len--;
-	}
-	return (1);
-}
-
-int	str_len(char *str)
+int	ft_strlen(char *str)
 {
 	int	len;
-
+	
 	len = 0;
 	while (*str)
 	{
@@ -29,29 +13,36 @@ int	str_len(char *str)
 	return (len);
 }
 
+int	ft_palindorm(int len, char *str)
+{
+	int	i;
+
+	i = 0;
+	while (i < len)
+	{
+		if (str[i] != str[len - 1])
+			return (0);
+		i++;
+		len--;
+	}
+	return (1);
+}
+
 int	main(void)
 {
-	char	*arr;
-	int		i;
 	int		len;
-	int		check;
-
-	arr = (char *)malloc(sizeof(*arr) + 1);
+	char	str[100000];
 
 	while (1)
 	{
-		scanf ("%s", arr);
-
-		if (arr[0] == '0')
-			break;
-		len = str_len(arr);
-		i = 0;
-		check = check_palin(len, arr);
-		if (check == 1)
-			printf ("yes\n");
+		scanf("%s", str);
+		if (str[0] == '0')
+			return (0);
+		len = ft_strlen(str);
+		if (ft_palindorm(len, str))
+			printf("yes\n");
 		else
-			printf ("no\n");
+			printf("no\n");
 	}
-	free(arr);
 	return (0);
 }
