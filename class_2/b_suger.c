ac7049eba687cd42e826605748976edcf654f994
Gyeongnam Kim <rudska252@naver.com>
2021-05-26 20:27:48 +0900

    fix-2839

--- class_2/b_suger.c
+++ class_2/b_suger.c
@@ -2,21 +2,22 @@
 
 int	main(void)
 {
-	int	n;
 	int	i;
 	int	j;
+	int	n;
+	int	sum;
 
 	scanf("%d", &n);
-
 	i = 0;
 	while (i < n)
 	{
 		j = 0;
 		while (j < n)
 		{
-			if ((3 * i) + (5 * j) == n)
+			if (3 * i + 5 * j  == n)
 			{
-				printf("%d\n", i + j);
+				sum = i + j;
+				printf("%d", sum);
 				return (0);
 			}
 			j++;
@@ -24,6 +25,5 @@ int	main(void)
 		i++;
 	}
 	printf("-1\n");
-
 	return (0);
 }
