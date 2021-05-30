b1f9e92cf00f2c9c38e9afe437b6fb775e2b30da
Gyeongnam Kim <rudska252@naver.com>
2021-05-31 01:28:45 +0900

    fail- 4949

--- class_2/f_balence_world.c
+++ class_2/f_balence_world.c
@@ -1,72 +1,90 @@
 #include <stdio.h>
 
-int	find_parentheses(char *str)
+int	ft_strlen(char *str)
+{
+	int	len;
+
+	len = 0;
+	while(*str)
+	{
+		str++;
+		len++;
+	}
+	return (len);
+}
+
+int	ft_check_parenthesis(int len, char *save)
 {
 	int	i;
 	int	j;
-	int	k;
-	int	count;
-	int	count2;
+	int	a_count;
+	int	b_count;
 
+	a_count = 0;
+	b_count = 0;
 	i = 0;
-	while (str[i])
+	while (save[i])
 	{
-		if (str[i] == ')')
+		if (save[i] == '(')
 		{
-			count = 0;
-			count2 = 0;
-			j = i - 1;
-			while (str[j] != '(')
-			{
-				count++;
-				j--;
-			}
-			k = i - 1;
-			while (str[k] != '[')
-			{
-				count2++;
-				k--;
-			}
-			if (count > count2)
+			if (save[j] == ']')
 				return (0);
+			a_count++;
 		}
-		if (str[i] == ']')
+		else if (save[i] == ')')
+			a_count--;
+		else if (save[i] == '[')
 		{
-			count = 0;
-			count2 = 0;
-			k = i - 1;
-			while (str[k] != '[')
-			{
-				count2++;
-				k--;
-			}
-			j = i - 1;
-			while (str[j] != '(')
-			{
-				count++;
-				j--;
-			}
-			if (count < count2)
+			if (save[j] == ')')
 				return (0);
+			b_count++;
 		}
-
+		else if (save[i] == ']')
+			b_count--;
+		if (a_count < 0 || b_count < 0)
+			return (0);
 		i++;
 	}
-
-	return (1);	//문제 없으면 통과	
+	if (a_count != 0 || b_count != 0)
+		return (0);
+	else
+		return (1);
 }
 
 int	main(void)
 {
-	char	str[100];
+	int		i;
+	int		j;
+	int		len;
 	int		ans;
+	char	save[102];
+	char	str[101];
 
-		scanf("%[^.]", str);
-		ans = find_parentheses(str);
-		
+	while (gets(str))
+	{
+		if (str[0] == '.')
+			return (0);
+		else
+		{
+		ans = 0;
+		len = ft_strlen(str);
+		i = 0;
+		j = 0;
+		while (i < len)
+		{
+			if (str[i] == '(' || str[i] == ')' || str[i] == '[' || str[i] == ']')
+			{
+				save[j] = str[i];
+				j++;
+			}
+			i++;
+		}
+		ans = ft_check_parenthesis(j, save);
 		if (ans == 1)
 			printf("yes\n");
 		else
 			printf("no\n");
-	return (0);
+		}
+	}
+	return(0);
 }
