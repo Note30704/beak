dc28a44523c3f3521754120e3691d93e30d26568
Gyeongnam Kim <rudska252@naver.com>
2021-06-05 17:07:01 +0900

    worse-10828

--- class_2/b_stack.c
+++ class_2/b_stack.c
@@ -1,50 +1,11 @@
 #include <stdio.h>
 
-int	g_arr[10000];
-int	g_i = 0;
+int	g_stack[100000];
+int	i = 0;
 
-void	push(int num)
+int	ft_strcmp (char *s1, char *s2)
 {
-	g_arr[g_i] = num;
-	g_i++;
-}
-
-void	pop(void)
-{
-	if (g_i != 0)
-	{
-		printf("%d\n", g_arr[g_i - 1]);
-		g_arr[g_i - 1] = 0;
-		g_i--;
-	}
-	else
-		printf("-1\n");
-}
-
-void	size(void)
-{
-	printf("%d\n", g_i);
-}
-
-void	empty(void)
-{
-	if (g_i != 0)
-		printf("0\n");
-	else
-		printf("1\n");
-}
-
-void	top(void)
-{
-	if (g_i != 0)
-		printf("%d\n", g_arr[g_i - 1]);
-	else
-		printf("-1\n");
-}
-
-int	ft_strcmp(char *s1, char *s2)
-{
-	while (*s1 && *s2)
+	while (*s1)
 	{
 		if (*s1 != *s2)
 			return (*s1 - *s2);
@@ -54,6 +15,44 @@ int	ft_strcmp(char *s1, char *s2)
 	return (*s1 - *s2);
 }
 
+void	ft_push (int num)
+{
+	g_stack[i] = num;
+	i++;
+}
+
+void	ft_pop(void)
+{
+	if (g_stack[i - 1] == 0)
+		printf("-1\n");
+	else
+	{
+		printf("%d\n", g_stack[i - 1]);
+		i--;
+	}
+}
+
+void	ft_size(void)
+{
+	printf("%d\n", i);
+}
+
+void	ft_empty(void)
+{
+	if (i > 0)
+		printf("0\n");
+	else
+		printf("1\n");
+}
+
+void	ft_top(void)
+{
+	if (i > 0)
+		printf("%d\n", g_stack[i - 1]);
+	else
+		printf("-1\n");
+}
+
 int	main(void)
 {
 	int		n;
@@ -61,24 +60,22 @@ int	main(void)
 	char	str[6];
 
 	scanf("%d", &n);
-	
 	while (n)
 	{
 		scanf("%s", str);
-		
-		if (ft_strcmp(str, "push") == 0)
+		if (ft_strcmp("push", str) == 0)
 		{
 			scanf("%d", &num);
-			push(num);
+			ft_push(num);
 		}
-		else if (ft_strcmp(str, "pop") == 0)
-			pop();
-		else if (ft_strcmp(str, "size") == 0)
-			size();
-		else if (ft_strcmp(str, "empty") == 0)
-			empty();
-		else if (ft_strcmp(str, "top") == 0)
-			top();
+		else if (ft_strcmp("pop", str) == 0)
+			ft_pop();
+		else if (ft_strcmp("size", str) == 0)
+			ft_size();
+		else if (ft_strcmp("empty", str) == 0)
+			ft_empty();
+		else if (ft_strcmp("top", str) == 0)
+			ft_top();
 		n--;
 	}
 	return (0);
