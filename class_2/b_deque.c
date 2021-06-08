fd57461db47a254aaee9d482b0fca2c16056866e
Gyeongnam Kim <rudska252@naver.com>
2021-06-08 17:49:49 +0900

    fix-10866

--- class_2/b_deque.c
+++ class_2/b_deque.c
@@ -1,74 +1,9 @@
 #include <stdio.h>
 
-int	g_arr[10000];
-int	g_i = 5000;
-int	g_j = 5001;
+int	deque[10000];
+int	f = 5000;
+int	b = 5001;
 int	count = 0;
-
-void	push_front(int num)
-{
-	g_arr[g_i] = num;
-	g_i--;
-	count++;
-}
-void	push_back(int num)
-{
-	g_arr[g_j] = num;
-	g_j++;
-	count++;
-}
-
-void	pop_front(void)
-{
-	if (count != 0)
-	{
-		printf("%d\n", g_arr[g_i + 1]);
-		g_i++;
-		count--;
-	}
-	else
-		printf("-1\n");
-}
-void	pop_back(void)
-{
-	if (count != 0)
-	{
-		printf("%d\n", g_arr[g_j - 1]);
-		g_j--;
-		count--;
-	}
-	else
-		printf("-1\n");
-}
-
-void	size(void)
-{
-	printf("%d\n", count);
-}
-
-void	empty(void)
-{
-	if (count != 0)
-		printf("0\n");
-	else
-		printf("1\n");
-}
-
-void	front(void)
-{
-	if (count != 0)
-		printf("%d\n", g_arr[g_i + 1]);
-	else
-		printf("-1\n");
-}
-
-void	back(void)
-{
-	if (count != 0)
-		printf("%d\n", g_arr[g_j - 1]);
-	else
-		printf("-1\n");
-}
 
 int	ft_strcmp(char *s1, char *s2)
 {
@@ -82,40 +17,105 @@ int	ft_strcmp(char *s1, char *s2)
 	return (*s1 - *s2);
 }
 
+void	ft_pushFront(int num)
+{
+	deque[f] = num;
+	count++;
+	f--;
+}
+
+void	ft_pushBack(int num)
+{
+	deque[b] = num;
+	count++;
+	b++;
+}
+
+void	ft_popFront(void)
+{
+	if (count == 0)
+		printf("-1\n");
+	else
+	{
+		printf("%d\n", deque[f + 1]);
+		f++;
+		count--;
+	}
+}
+
+void	ft_popBack(void)
+{
+	if (count == 0)
+		printf("-1\n");
+	else
+	{
+		printf("%d\n", deque[b - 1]);
+		b--;
+		count--;
+	}
+}
+
+void	ft_size(void)
+{
+	printf("%d\n", count);
+}
+
+void	ft_empty(void)
+{
+	if (count != 0)
+		printf("0\n");
+	else
+		printf("1\n");
+}
+
+void	ft_front(void)
+{
+	if (count == 0)
+		printf("-1\n");
+	else
+		printf("%d\n", deque[f + 1]);
+}
+
+void	ft_back(void)
+{
+	if (count == 0)
+		printf("-1\n");
+	else
+		printf("%d\n", deque[b - 1]);
+}
+
 int	main(void)
 {
 	int		n;
 	int		num;
-	char	str[11];
+	char	str[12];
 
 	scanf("%d", &n);
-	
 	while (n)
 	{
 		scanf("%s", str);
-		
-		if (ft_strcmp(str, "push_front") == 0)
+		if (ft_strcmp("push_front", str) == 0)
 		{
 			scanf("%d", &num);
-			push_front(num);
+			ft_pushFront(num);
 		}
-		else if (ft_strcmp(str, "push_back") == 0)
+		else if (ft_strcmp("push_back", str) == 0)
 		{
 			scanf("%d", &num);
-			push_back(num);
+			ft_pushBack(num);
 		}
-		else if (ft_strcmp(str, "pop_front") == 0)
-			pop_front();
-		else if (ft_strcmp(str, "pop_back") == 0)
-			pop_back();
-		else if (ft_strcmp(str, "size") == 0)
-			size();
-		else if (ft_strcmp(str, "empty") == 0)
-			empty();
-		else if (ft_strcmp(str, "front") == 0)
-			front();
-		else if (ft_strcmp(str, "back") == 0)
-			back();
+		else if (ft_strcmp("pop_front", str) == 0)
+			ft_popFront();
+		else if (ft_strcmp("pop_back", str) == 0)
+			ft_popBack();
+		else if (ft_strcmp("size", str) == 0)
+			ft_size();
+		else if (ft_strcmp("empty", str) == 0)
+			ft_empty();
+		else if (ft_strcmp("front", str) == 0)
+			ft_front();
+		else if (ft_strcmp("back", str) == 0)
+			ft_back();
 		n--;
 	}
 	return (0);
