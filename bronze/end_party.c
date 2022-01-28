fef8f9dc4219dc157be898b7c64ceac4c56572f9
Gyeongnam Kim <rudska252@naver.com>
2022-01-28 20:56:16 +0900

    bronze-2845

---
+++ bronze/end_party.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int	main(void)
+{
+	int	i;
+	int	L;
+	int	P;
+	int	LP;
+	int	article[5];
+
+	scanf("%d %d", &L, &P);
+	
+	i = 0;
+	while(i < 5)
+	{
+		scanf("%d", &article[i]);
+		i++;
+	}
+
+	LP = L * P;
+	i = 0;
+	while (i < 5)
+	{
+		printf("%d ", article[i] - LP);
+		i++;
+	}
+	return (0);
+}
